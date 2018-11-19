b3e618b0b896546b9f73e9d1e05228a4c832bef7
Xin <xwang@hudsonhs.com>
2018-11-19 23:12:06 +0000

    Unit 2

--- js/script.js
+++ js/script.js
@@ -7,9 +7,47 @@
 var userChoice = "";
 var computerChoice = "";
 var winner = "";
+var w = "User Win";
+var t = "Tie";
+var c = "Computer Win";
 
 $("#shoot").click(function(){
     $("#userChoice").text($("#input").val());
+    var randomNumber = Math.random();
+    userChoice = $("#input").val();
+    
+    if (randomNumber > 0 && randomNumber < 0.33) {
+        computerChoice = "scissors";
+        $("#computerChoice").text(computerChoice);
+    } else if (randomNumber > 0.33 && randomNumber < 0.66) {
+        computerChoice = "rock";
+        $("#computerChoice").text(computerChoice);
+    } else if (randomNumber > 0.66 && randomNumber < 1) {
+        computerChoice = "paper";
+        $("#computerChoice").text(computerChoice);
+    }
+    
+    if (userChoice === "scissors" && computerChoice === "scissors") {
+        $("#result").text(t);
+    } else if (userChoice === "rock" && computerChoice === "rock") {
+        $("#result").text(t);
+    } else if (userChoice === "paper" && computerChoice === "paper") {
+        $("#result").text(t);
+    } else if (userChoice === "scissors" && computerChoice === "rock") {
+        $("#result").text(c);
+    } else if (userChoice === "rock" && computerChoice === "paper") {
+        $("#result").text(c);
+    } else if (userChoice === "paper" && computerChoice === "scissors") {
+        $("#result").text(c);
+    } else if (userChoice === "scissors" && computerChoice === "paper") {
+        $("#result").text(w);
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+        $("#result").text(w);
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+        $("#result").text(w);
+    } else {
+        $("#result").text('Please input "rock" or "paper" or "scissors"!');
+    }
 });
 
 
